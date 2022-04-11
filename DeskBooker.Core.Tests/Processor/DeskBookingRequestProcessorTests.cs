80b6f4781d9ff0098f15d06651842bb4e3f9e2e2
Mike Mahachi <mike@skrmiish.com>
2022-04-11 19:52:32 +0200

    Added test that returns results with values and second that thrws an exception when null is passed

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,50 @@
+﻿using DeskBooker.Core.Domain;
+using NUnit.Framework;
+using System;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        private DeskBookingRequestProcessor _processor;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _processor = new DeskBookingRequestProcessor();
+        }
+
+        [Test]
+        public void ShouldReturnDeskBookingResultWithValues()
+        {
+            // Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Mike",
+                LastName = "Mahachi",
+                Email = "iron@mike.com",
+                Date = new DateTime(2022, 03, 23)
+            };
+
+
+            // Act
+            DeskBookingResult result = _processor.BookDesk(request);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.FirstName, Is.EqualTo(request.FirstName));
+            Assert.That(result.LastName, Is.EqualTo(request.LastName));
+            Assert.That(result.Email, Is.EqualTo(request.Email));
+            Assert.That(result.Date, Is.EqualTo(request.Date));
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfResultIsNull()
+        {
+
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
+
+            Assert.That(exception.ParamName,Is.EqualTo("request"));
+        }
+    }
+}
